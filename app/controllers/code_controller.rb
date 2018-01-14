0c2f0b5d344b9f2724477f65d422d1dc9ad1ad81
Pankaj Sharma <sharmapankaj090@gmail.com>
2018-01-14 22:47:38 +0530

    edits

--- app/controllers/code_controller.rb
+++ app/controllers/code_controller.rb
@@ -39,46 +39,56 @@ class CodeController < ApplicationController
  		response = Net::HTTP.post_form(uri, {'sourceCode' => params["sc"] , 'compilerId' => params["Compiler"], 'input' => params["testcase"]})
  		
  			
- 			returned_id=JSON.parse(response.body)["id"].to_s
-					returned_status=-1	
+ 		returned_id=JSON.parse(response.body)["id"].to_s
+		returned_status=-1	
 		 		
 		 				
 					
-							url = URI.parse("http://0fa75163.compilers.sphere-engine.com/api/v3/submissions/"+returned_id+"?access_token=6bf3291fc2e34e712d804efe8a198e11")
-							req = Net::HTTP::Get.new(url.to_s)
-							url.query = URI.encode_www_form({  "access_token" => "6bf3291fc2e34e712d804efe8a198e11" ,
-															   "withSource" => true, 
-						                                       "withInput" => true, 
-						                                       "withOutput" => true ,
-						                                       "withStderr" => true,
-						                                       "withCmpinfo" => true
-						                                    })
-						loop do 
-						 	sleep(6)
-						 	  if Net::HTTP.get_response(url).body != nil
-				 			   break
-								  end
-							end
+		url = URI.parse("http://0fa75163.compilers.sphere-engine.com/api/v3/submissions/"+returned_id+"?access_token=6bf3291fc2e34e712d804efe8a198e11")
+		req = Net::HTTP::Get.new(url.to_s)
+		url.query = URI.encode_www_form({  "access_token" => "6bf3291fc2e34e712d804efe8a198e11" ,
+											"withSource" => true, 
+						                    "withInput" => true, 
+						                    "withOutput" => true ,
+						                    "withStderr" => true,
+						                    "withCmpinfo" => true
+						                })
+		out= JSON.parse Net::HTTP.get_response(url).body
+		loop do 
+			sleep(6)
+			out= JSON.parse Net::HTTP.get_response(url).body
+			p "response from ideone: "+out.to_s
+			if out["status"] == 0
+				break
+			end
+		end
 
-     						out= JSON.parse Net::HTTP.get_response(url).body
-					
-							#PRINT RESPONSE  res.body	
-							#out = JSON.parse(res.body)
-							returned_status = out["status"]
-							returned_result = out["result"]	
-					@ret_view = out["output"]
-			
-			puts out["output"]
-			 if out["result"] == 15
-			 render plain:  @ret_view					
-		   	#	redirect_to action: :view
-		   	end  
-		   	
 		
-													
-#	binding.pry
+		returned_status = out["status"]
+		returned_result = out["result"]	
+		p "returned is :->"
+		p "returned_status: "+out["status"].to_s
+		p "returned_result: "+out["result"].to_s
+		@ret_view = out["output"]
+			
+		puts out["output"]
+
+			if out["result"] == 15
+			    render plain:  @ret_view					
+			elsif out["result"] == 11
+				render plain: "Compilation Error"
+			elsif out["result"] == 12
+				render plain: "Runtime Error"
+			elsif out["result"] == 13
+				render plain: "Time Limit Exceeded"		
+			elsif out["result"] == 17
+				render plain: "Memory Limit Exceeded"	
+			elsif out["result"] == 19
+				render plain: "Illegal System Call"	
+			elsif out["result"] == 20
+				render plain: "Internal Error	"	
+		end  
+		   	
 	end
 
 end
-
-
