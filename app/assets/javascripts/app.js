e68f7756d8e67ea51838db7d6b341e903fab1754
Pankaj Sharma <sharmapankaj090@gmail.com>
2018-08-07 00:24:21 +0530

    last edits

--- app/assets/javascripts/app.js
+++
@@ -1,5 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-windows.load('particles-js', 'app/assets/javascripts/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,5 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree 
-//= require particles.min
-//= require particles
+//= stats
+//= app
